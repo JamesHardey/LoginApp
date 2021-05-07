e24d5aeba75e1a4377d71b0a6a6a3cc3fdd07e65
James <jamesade646@gmail.com>
2021-05-07 11:44:44 -0400

    front design finished

---
+++ app/src/main/java/com/example/myapplication/DummyData.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+object DummyData {
+    val facts = listOf<FactModel>(
+        FactModel("Kotlin",
+        R.drawable.kotlin_logo,
+        "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
+                "to name"
+
+        ),
+
+        FactModel(
+            "Java",
+            R.drawable.ic_java_logo,
+            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
+                    "to name"
+        ),
+
+        FactModel(
+            "Python",
+            R.drawable.python_logo,
+            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
+                    "to name"
+        ),
+
+        FactModel(
+            "Javascript",
+            R.drawable.js_logo,
+            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
+                    "to name"
+        ),
+
+        FactModel(
+            "PHP",
+            R.drawable.php_logo,
+            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
+                    "to name"
+        )
+
+    )
+}
---
+++ app/src/main/java/com/example/myapplication/FactModel.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+data class FactModel(val name:String,val logo:Int,val fact:String) {
+}
