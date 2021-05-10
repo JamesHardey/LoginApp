8ede6545bb230c42368efd6ca278bafe9bcb5efe
James <jamesade646@gmail.com>
2021-05-09 21:21:13 -0400

    new changes (logo changed)

--- app/src/main/java/com/example/myapplication/DummyData.kt
+++ app/src/main/java/com/example/myapplication/DummyData.kt
@@ -2,37 +2,90 @@ package com.example.myapplication
 
 object DummyData {
     val facts = arrayOf<FactModel>(
-        FactModel("Kotlin",
-        R.drawable.kotlin_logo,
-        "Headquartered in Dallas,Texas"
+
+        FactModel("AT&T",
+        R.drawable.at_t,
+        "AT&T’s elegant and stylish telecom logo is one of the most recognized corporate symbols in the world. " +
+                "The globe in AT&T’s logo is intended to convey that the company has a strong global presence."
         ),
 
         FactModel(
-            "Java",
-            R.drawable.ic_java_logo,
-            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
-                    "to name"
+            "China Mobile Ltd",
+            R.drawable.china_mobile,
+            "Headquartered in Hong Kong, China Mobile Ltd. is one of the biggest telecommunication service providers in the world." +
+                    "Its logo brilliantly presents the telecommunication company’s business message to its customers. In addition, " +
+                    "the use of blue color in the logo invokes the emotions of trustworthiness and stability."
         ),
 
         FactModel(
-            "Python",
-            R.drawable.python_logo,
-            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
-                    "to name"
+            "Telstra Telecom Logo",
+            R.drawable.telstra,
+            "Based in Melbourne, Australia, Telstra is one of the world’s most popular telecom and information" +
+                    " service providers. Telstra’s stylish and colorful telecom logo is aimed at capturing the " +
+                    "diversity of company’s products, services and customers. The designer uses only two colors to convey a message." +
+                    " Yellow stands for friendly customer services from the company." +
+                    "Based on their logo one can easily infer that choice of colors matter a lot for it’s success."
         ),
 
         FactModel(
-            "Javascript",
-            R.drawable.js_logo,
-            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
-                    "to name"
+            "Singtel Telecom",
+            R.drawable.singtel,
+            "Singapore Telecommunications Limited, popularly called Singtel is headquartered in Singapore. " +
+                    "It ranks alongside some of the largest mobile network operators in the entire world. Marked by a " +
+                    "signature red arc, Singtel’s logo reflects innovation and the ongoing evolution of the company. " +
+                    "The creative use of typeface in the logo is sans serif for a consumer friendly environment of the company."
+
         ),
 
         FactModel(
-            "PHP",
-            R.drawable.php_logo,
-            "The name comes from Kotlin Island,near St. Petersburg. Andrey Breslav mentioned that the team decided" +
-                    "to name"
+            "Nippon Telegraph & Telephone Corp.",
+            R.drawable.nippon,
+            "Nippon Telegraph & Telephone Corp. is headquartered in Tokyo, Japan and is mainly engaged in telecom services. " +
+                    "The company’s simple and elegant logo represents " +
+                    "the expanding breadth and depth of services that the company aims to provide to its customers."
+        ),
+
+        FactModel(
+                "America Movil Telecom Logo",
+        R.drawable.movil,
+        "Headquartered in Mexico City, Mexico, America Movil is one of the popular wireless telecommunications service providers." +
+                " America Movil’s stylish logo is a swirling design that indicates growth of the company’s business."
+        ),
+
+        FactModel(
+            "Deutsche Telekom Logo",
+            R.drawable.deutsche,
+            "Operating out of Bonn, Germany, Deutsche Telekom is a leading telecommunication, information technology, " +
+                    "information, entertainment and multimedia service provider. Its logo has the letter T in red. The letter dominates the design." +
+                    " Can you see a human face in the T shape? That makes this logo memorable. " +
+                    "Such unique logo designs enhance visibility of your brand."
+        ),
+
+
+        FactModel(
+            "Telefonica Communication Logo",
+            R.drawable.telefonica,
+            "Based in Madrid, Spain, Telefonica is an integrated telecom operator popular for offering exceptional " +
+                    "communication and information services. The Telefonica logo has a large circle and small colored balls in T shape. " +
+                    "These elements stand for a broad spectrum of telecom services." +
+                    " This professionally designed logo thus convey the company’s vision."
+        ),
+
+        FactModel(
+            "SoftBank Telecommunication Logo",
+            R.drawable.softbank,
+            "SoftBank Corp. is headquartered in Tokyo, Japan and provides information technology and telecommunication services." +
+                    "It has a fairly simple telecommunication logo. It has a yellow equation sign that stands " +
+                    "for quality and trustworthy services of the company."
+        ),
+
+        FactModel(
+            "Vodafone Telecommunication Logo",
+            R.drawable.vodafone,
+            "Vodafone Group is headquartered in UK and specializes in providing mobile telecom services." +
+                    " Its colorful speech mark logo symbolizes conversation and voice communication. " +
+                    "The designer has used red color for quick visibility. But red is also a color for passion, " +
+                    "which the consumers show while communicating with their peers. Such unique logos are important to an organization."
         )
 
     )
